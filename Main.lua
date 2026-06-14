4ca8c12e92db5703e529c80ae83e36f36d7e256a
Nicklewko <nicklew.lewkowicz@gmail.com>
2026-06-14 10:17:42 +0200

    Implement noclip and enhance stealing mechanics
    Added noclip functionality and improved target selection for stealing fruits.

--- Main.lua
+++ Main.lua
@@ -1,5 +1,9 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local RunService = game:GetService("RunService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local gardenZoneData = ReplicatedStorage.GardenZoneData
+local night = ReplicatedStorage.Night
 
 local player = game.Players.LocalPlayer
 local cam = workspace.CurrentCamera
@@ -15,8 +19,13 @@ local seeds = workspace.Map.SeedPackSpawnServerLocations
 
 local collectSeeds = false
 local collectDropped = false
+local noclip = false
+
 local stealTarget = nil
+local stealTargetToggled = false
+
 local oldPos = nil
+
 local queue = {}
 
 local Window = Rayfield:CreateWindow({
@@ -43,7 +52,7 @@ Rayfield:Notify({
 	Image = 4483362458,
 })
 
-local function NoclipLoop()
+local function noclipLoop()
 	local character = player.Character
 	if character ~= nil then
 		for _, child in pairs(character:GetDescendants()) do
@@ -84,7 +93,6 @@ local function collect(p)
 	while prompt.Parent do
 		char:PivotTo(CFrame.new(targetPos - Vector3.new(0, 4, 0)))
 		fireproximityprompt(prompt)
-		--NoclipLoop()
 		task.wait()
 	end
 
@@ -129,10 +137,51 @@ end
 
 local function getPlayerList()
 	local pt = {}
-	for _, player in pairs(game.Players:GetChildren()) do
-		table.insert(pt, player.Name)
+	for _, p in pairs(game.Players:GetChildren()) do
+		if p == player then continue end
+		table.insert(pt, p.Name)
 	end
 	return pt
+end
+
+local function getPlayerName(text)
+	local playerName = text:match("%] (.-)'s garden")
+	return playerName or nil
+end
+
+local function getTargetGarden(t)
+	for _, plot in pairs(workspace.Gradens:GetChildren()) do
+		local nameTextLabel = plot.Signs.Garden.CorePart.SurfaceGui.Player.TextLabel
+		local n = getPlayerName(nameTextLabel.Text)
+		if n == t then
+			return plot
+		end
+	end
+end
+
+local function getTargetFruit(t)
+	local garden = getTargetGarden(t)
+	if not garden then return end
+
+	local plants = garden.Plants:GetChildren()
+
+	for _, target in pairs(plants) do
+		local fruits = target:FindFirstChild("Fruits")
+		if not fruits then continue end
+		for _, targetFruit in pairs(fruits:GetChidren()) do
+			if targetFruit.HarvestPart and FindFirstDescendantOfClass(targetFruit.HarvestPart, "ProximityPrompt") then
+				return targetFruit
+			end
+		end
+	end
+end
+
+local function isInGarden(t)
+	return gardenZoneData:FindFirstChild(t)
+end
+
+local function canSteal(t)
+	return isInGarden(t) or night.Value
 end
 		
 task.spawn(function()
@@ -141,7 +190,13 @@ task.spawn(function()
 			print(i, v.t)
 		end
 
-		if #queue > 0 then
+		if stealTarget and stealTargetToggled and not canSteal(stealTarget) then
+			local item = getTargetFruit(stealTarget)
+				
+			if item and item.Parent then
+				collect(item)
+			end
+		elseif #queue > 0 then
 			local item = table.remove(queue, 1)
 
 			if item and item.m and item.m.Parent then
@@ -149,7 +204,11 @@ task.spawn(function()
 			end
 		end
 
-		task.wait(0.1)
+		if noclip then
+			noclipLoop()
+		end
+		
+		task.wait()
 	end
 end)
 
@@ -172,6 +231,16 @@ local AutoMainSection = AutoTab:CreateSection("Main")
 
 local StealTab = Window:CreateTab("Steal", 4483362458)
 local StealTargetSection = StealTab:CreateSection("Target")
+
+local NoclipToggle = PlayerTab:CreateToggle({
+	Name = "Noclip",
+	CurrentValue = noclip,
+	Flag = "noclip",
+	Callback = function(Value)
+		noclip = Value
+	end,
+})
+
 local StealTargetSelect = StealTab:CreateDropdown({
    Name = "Select Target",
    Options = {},
@@ -181,6 +250,15 @@ local StealTargetSelect = StealTab:CreateDropdown({
    Callback = function(Options)
 		stealTarget = Options[1]
    end,
+})
+
+local StealTargetToggled = StealTab:CreateToggle({
+	Name = "Steal Target",
+	CurrentValue = stealTargetToggled,
+	Flag = "stealtargettoggled",
+	Callback = function(Value)
+		stealTargetToggled = Value
+	end,
 })
 
 local AutoCollectDroppedToggle = AutoTab:CreateToggle({
